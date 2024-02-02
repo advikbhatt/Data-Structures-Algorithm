62e44366f7bd0e8c99af899bce582a101b33818d
ADVIK BHATT <96647464+advikbhatt@users.noreply.github.com>
2024-02-02 15:36:24 +0530

    Add files via upload

---
+++ Linked_List/linked_list_creation_traversal.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+void traversal(struct node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+int main()
+{
+    struct node *head;
+    head = (struct node *)malloc(sizeof(struct node));
+    struct node *second;
+    second = (struct node *)malloc(sizeof(struct node));
+    struct node *third;
+    third = (struct node *)malloc(sizeof(struct node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 23;
+    second->next = third;
+
+    third->data = 34;
+    third->next = NULL;
+
+    traversal(head);
+
+    return 0;
+}
